3e5fa9d2d0b62294b8a7237266db3b330ce3439b
0xdedsec <0xdedsec@gmail.com>
2021-12-01 15:20:22 -0500

    solved part2 of day 1

--- 01_SonarSweep/sonar_sweep.v
+++ 01_SonarSweep/sonar_sweep.v
@@ -3,22 +3,43 @@ module main
 
 import os
 
+fn larger_sums(data []int) int {
+	mut answer := 0
+	mut prev := -1
+
+	for i in data {
+		if prev >= 0 && i > prev {
+			answer++
+		}
+		prev = i
+	}
+	return answer
+}
+
 fn main() {
 	file := os.read_lines("input.txt") or {
 		println(err)
 		exit(-1)
 	}
 
-	mut answer := 0
-	mut prev := -1
-
+	mut data := []int{}
 	for line in file {
-		x := line.int()
-		if prev >= 0 && x > prev {
-			answer++
-		}
-		prev = x
+		data << line.int()
 	}
 
-	println("Part1 answer: $answer")
+	res1 := larger_sums(data)
+
+	println("Part1 answer: $res1") 
+
+	mut sums := []int{}
+
+	for key, _ in data {
+		if key < data.len - 2 {
+			sums << data[key] + data[key+1] + data[key+2]
+		}
+	}
+
+	res2 := larger_sums(sums)
+
+	println("Part2 answer: $res2")
 }
